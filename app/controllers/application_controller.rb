a768cfc22c7d764436b98a35527266ab5608858d
intelsy <intel.sy.408@gmail.com>
2018-08-14 08:31:38 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+    self.abstract_class = true
+  
+  def hello
+    render html: "hello, world!"
+  end
+  
 end
